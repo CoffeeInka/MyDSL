c9feb49e46a1a5d244f2f9a1e15e12e65243e741
CoffeeInka <inna.shevchenko@gmail.com>
2017-10-17 14:33:00 +0200

    still selenide project

---
+++ src/test/java/com/google/GoogleSearchTest.java
@@ -0,0 +1,48 @@
+package com.google;
+
+import com.codeborne.selenide.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.*;
+
+
+import static org.junit.Assert.*;
+
+
+public class GoogleSearchTest {
+
+
+    @Test
+    public void testSearchThenFollowLink() {
+        open("http://www.google.com");
+
+        search("Selenium automates browsers");
+        assertResultsAmount(10);
+        results.first().shouldHave(text("Selenium automates browsers"));
+
+        followLink(0);
+        $("#mainContent>h2").shouldBe(visible);
+        assertEquals(url(), "http://www.seleniumhq.org/");
+    }
+
+    private void search(String query) {
+        $(By.name("q")).setValue(query).pressEnter();
+    }
+
+    public void followLink(int index) {
+        results.get(index).find(".r>a").click();
+    }
+
+    public ElementsCollection results = $$(".srg>.g");
+
+    public void assertResultsAmount(int resultsAmount) {
+        results.shouldHave(size(resultsAmount));
+    }
+}
